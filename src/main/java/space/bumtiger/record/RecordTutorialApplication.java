0c669e0ec61c65233903cc0a43d1f75f16760aef
osparking <jbpark03@gmail.com>
2023-06-18 23:36:30 +0900

    initial status of record class tutorial project

---
+++ src/main/java/space/bumtiger/record/RecordTutorialApplication.java
@@ -0,0 +1,13 @@
+package space.bumtiger.record;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecordTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecordTutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/space/bumtiger/record/RecordTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package space.bumtiger.record;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecordTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
