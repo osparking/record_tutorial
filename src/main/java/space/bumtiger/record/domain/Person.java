720fe15e7b73b03c993e0d428db90a644e62540d
osparking <jbpark03@gmail.com>
2023-06-18 23:46:14 +0900

    Person POJO data carrier class

---
+++ src/main/java/space/bumtiger/record/domain/Person.java
@@ -0,0 +1,54 @@
+package space.bumtiger.record.domain;
+import java.util.Objects;
+
+public class Person {
+	private int id;
+	private String name;
+	
+	public Person() {
+	}
+	
+	public Person(int id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, name);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Person other = (Person) obj;
+		return id == other.id && Objects.equals(name, other.name);
+	}
+
+	@Override
+	public String toString() {
+		return "Person [id=" + id + ", name=" + name + "]";
+	}
+
+}
