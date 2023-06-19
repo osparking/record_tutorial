5543812dde723016be4d375c24dc1abb499dbbaf
osparking <jbpark03@gmail.com>
2023-06-19 15:35:39 +0900

    compact constructor for invoice record

--- src/main/java/space/bumtiger/record/domain/Invoice.java
+++ src/main/java/space/bumtiger/record/domain/Invoice.java
@@ -9,14 +9,12 @@ public record Invoice(String id, float amount) {
 	static Calendar instance = Calendar.getInstance();
 	static String monStr = String.valueOf(instance.get(Calendar.MONTH) + 1);
 	static String monStrPadded = StringUtils.leftPad(monStr, 2, "0");
-	
-	static String prefix = String
-			.valueOf(instance.get(Calendar.YEAR))
+
+	static String prefix = String.valueOf(instance.get(Calendar.YEAR))
 			+ monStrPadded;
 
-	public Invoice(String id, float amount) {
-		this.id = prefix + "_" + id.trim();
-		this.amount = amount;
+	public Invoice {
+		id = prefix + id.trim();
+		// amount=amount; // 효과 없는 문장
 	}
-
 }
