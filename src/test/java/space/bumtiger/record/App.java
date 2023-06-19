9fb48972e92b97832e0fa3b530c9dbcb746801b3
osparking <jbpark03@gmail.com>
2023-06-19 22:43:20 +0900

    typical usage example of a record

--- src/test/java/space/bumtiger/record/App.java
+++ src/test/java/space/bumtiger/record/App.java
@@ -1,16 +1,20 @@
 package space.bumtiger.record;
 
 import lombok.extern.slf4j.Slf4j;
-import space.bumtiger.record.domain.Person;
+import space.bumtiger.record.domain.Invoice;
 
 @Slf4j
 public class App {
 	public static void main(String[] args) {
-		Person he = new Person(1, "홍길동");
-		Person she = new Person(2, "장화홍련");
+		float[] amt = {400f, 600f, 300f, 700f, 600f};
+		Invoice[] invoice = new Invoice[5];
 		
-		App.log.info(he.toString());
-		App.log.info(he.equals(she) ? "동일" : "상이");
-		App.log.info(he.name());
+		for (int i = 0; i <invoice.length; i++) {
+			invoice[i] = new Invoice(String.valueOf(i+1), amt[i]);
+		}
+		
+		for (int i = 0; i <invoice.length; i++) {
+			App.log.info(invoice[i].toString());
+		}
 	}
 }
